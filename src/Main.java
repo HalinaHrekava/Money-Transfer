dbb058c44c82dda71c94eb63b14984b04c5ba0aa
Halina Hrekava <gluk2019@mail.ru>
2021-03-11 09:56:47 +0300

    Int total

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long account = 2_000_000_000;
+        long transfer = 500_000_000;
+        long total = account + transfer;
+        System.out.println("Total: " + total);
+    }
+}
